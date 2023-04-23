726ba231b82d3cd65105e662fa76e9f227644c25
yaoboyu <1712683212@qq.com>
2023-04-23 16:35:52 +0800

    单词详细释义和单词例句实现

--- webapp/src/main/java/com/webapp/webapp/Service/StudyService.java
+++ webapp/src/main/java/com/webapp/webapp/Service/StudyService.java
@@ -1,5 +1,6 @@
 package com.webapp.webapp.Service;
 
+import com.webapp.webapp.Dto.WordSentence;
 import com.webapp.webapp.Pojo.Word;
 
 import java.util.List;
@@ -10,7 +11,9 @@ public interface StudyService {
      * @param UserName 用户名
      * @return 单词词组
      */
-    public List<Word> getWordsByUserName(String UserName) throws Exception;
+    List<Word> getWordsByUserName(String UserName) throws Exception;
 
     void updateWordStatus(String userName, Integer wordId, Integer status) throws Exception;
+    WordSentence wordSentence(String word);
+    String wordDetail(String word);
 }
--- webapp/src/main/java/com/webapp/webapp/WebappApplication.java
+++ webapp/src/main/java/com/webapp/webapp/WebappApplication.java
@@ -11,5 +11,4 @@ public class WebappApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
     }
-
 }
